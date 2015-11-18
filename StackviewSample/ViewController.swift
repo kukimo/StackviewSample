6a2514e96aa364f413940c238f790eda62a49079
Suzuki Shingo <kuma.kinoko.momo@gmail.com>
2015-11-18 23:38:22 +0900

    Qiita用サンプル

---
+++ StackviewSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackviewSample
+//
+//  Created by 鈴木 慎吾 on 2015/11/15.
+//  Copyright © 2015年 JPOP-NEWS.net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
